6967408e443d16f77112fce32f0556e288245fdd
unknown <aru.krishnan2015@gmail.com>
2021-12-01 20:07:06 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const productRoutes = require("./routes/productRoutes");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API running..." });
+});
+
+app.use("/api/products", productRoutes);
+
+app.get('/', (req,res) => {
+  res.send('Hello to Shopping Cart');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
